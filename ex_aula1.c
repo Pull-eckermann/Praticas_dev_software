153bb48d9fc965346c4b11d53429bca3b0ac1672
ERICK ECKERMANN CARDOSO <eec18@inf.ufpr.br>
2022-10-19 10:57:31 -0300

    Só falta make

--- ex_aula1.c
+++ ex_aula1.c
@@ -5,28 +5,46 @@
 //Struct que representa uma pessoa
 typedef struct person{
     char name[1024];
-    unsigned int cpf;
+    long int cpf;
     int age;
 } person;
 
 //Função que consulta cpf e valida
-int validarCPF(unsigned int cpf){
-    int soma;
-    int div = 1000;
-    int div2 = 100;
-    for(int i = 10; i >=2; i--){
-        soma = cpf % div;
-        soma /=div2
-        soma *= i;
-        div *= 10;
-        div2 *= 10;
+int validarCPF(long int cpf){
+    //Primeira parte da validação
+    int soma = 0;
+    long int aux = cpf / 100;
+    int dig;
+    for(int i = 2; i <=10; i++){
+        dig = aux % 10;
+        aux /= 10;
+        soma = (dig * i) + soma;
     }
-    soma = soma * 10;
+    soma *= 10;
     int resto = soma % 11;
-    
-
-
-    return 1;
+    if(resto == 10)
+        resto = 0;
+    dig = cpf % 100;
+    dig /= 10;
+    if(dig == resto){ //Inicia a segunda parte da validação
+	soma = 0;
+	aux = cpf / 10;
+	for(int i = 2; i <= 11; i++){
+            dig = aux % 10;
+	    aux /= 10;
+	    soma = (dig * i) + soma;
+	}
+	soma *= 10;
+	resto = soma % 11;
+	if(resto == 10)
+	    resto = 0;
+	dig = cpf % 10;
+        if(dig == resto)
+	    return 1;
+	else
+	    return 0;
+    }else
+        return 0;
 }
 //Função responsável por colher nome do usuário do tipo person
 void *leNome(char *teste){
@@ -34,16 +52,17 @@ void *leNome(char *teste){
     printf("Digite o nome do usuário: ");
     scanf("%s",nome);
     memcpy(teste,nome,1024);
-
 }
 //Função responsável por colher o cpf
-unsigned int leCPF(){
-    unsigned int cpf;
+long int leCPF(){
+    long int cpf;
     printf("Digite o CPF do usuário: ");
-    scanf("%u",&cpf);
+    scanf("%ld",&cpf);
+    printf("%ld\n",cpf);
     while(!(validarCPF(cpf))){
         printf("CPF inválido, digite novamente:");
-        scanf("%u",&cpf);
+        scanf("%ld",&cpf);
+	printf("%ld\n",cpf);
         if (validarCPF(cpf))
             return cpf;
     }
@@ -61,7 +80,7 @@ void imprimeLista(person *p_list){
     for(int i = 0; i < 3; i++){
         printf("\nCadastro %d \n",i+1);
         printf("Nome: %s \n",p_list[i].name);
-        printf("CPF: %u \n",p_list[i].cpf);
+        printf("CPF: %ld \n",p_list[i].cpf);
         printf("Idade: %d \n", p_list[i].age);
     }
 }
@@ -72,7 +91,7 @@ void main(){
     //Colhe os dados de cada usuário
     for (int i = 0; i < 3; i++){
         printf("Insira os dados do usuário %d \n",i+1);
-        leNome(nome);
+	leNome(nome);
         memcpy(p_list[i].name,nome,1024);
         p_list[i].cpf = leCPF();
         p_list[i].age = leIdade();
